cf23f73e9a65ddc092dc5188f1434c8c9359e231
punchgit77 <panchmukh7777@gmail.com>
2021-07-15 20:20:44 +0530

    t4 part

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,32 +1,52 @@
-import React,{ useEffect,useState } from 'react'
+/* eslint-disable no-console */
+import  React,{ useEffect,useReducer } from 'react'
 import { useParams } from 'react-router-dom'
 import { apiGet } from '../misc/config';
 
+
+const initialState={
+
+ show:null,
+ isloading:true,
+ error:null
+}
+
+const reducer=(prevState,action)=>{
+    switch(action.type){
+        // eslint-disable-next-line no-undef
+        case 'FETCH_SUCCESS':{ return {isloading:false, error:null, show:show.action}}
+
+        case 'FETCH_FAILED':{ return {...prevState,isloading:false,error:action.error}}
+
+        default : return prevState;
+    }
+}
+
+
 const Show = () => {
+    
+    const { id }=useParams();
 
-    const {id}=useParams();
-
-
-    const [show,setshow]= useState(null);
-    const [isloading,setisloading]=useState(true);
-    const [error,seterror]=useState(null);
+    const [{show,isloading,error},dispatch]=useReducer(reducer,initialState);
+    // const [show,setshow]= useState(null);
+    // const [isloading,setisloading]=useState(true);
+    // const [error,seterror]=useState(null);
+    
 
     useEffect(()=>{
         let isMounted=true;
-        setisloading(false);
           apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results=>{
               
             setTimeout(()=>{
                 if(isMounted){
-            setshow(results);
-            setisloading(false);
-        }
+               dispatch({type:'FETCH_SUCCESS',show:results})
+                }
         },2000)
         }
               ).catch(err=>{
                   if(isMounted){
-                      seterror(err.message);
-                      setisloading(false);
+                    dispatch({type:'FETCH_FAILED',error:err.message})
+
                   }
               })
 
@@ -36,7 +56,9 @@ const Show = () => {
     },[id])
 
     // eslint-disable-next-line no-console
-    console.log('show',show)
+    console.log('show',show);
+    // eslint-disable-next-line no-console
+    console.log('isloading',isloading);
 
     if(isloading){
         return <div>Data is being loaded</div>
@@ -52,4 +74,4 @@ const Show = () => {
     )
 }
 
-export default Show
+export default Show;
